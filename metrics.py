ade4df84a56cdf063f6b4664bc3a1aabcfa44e01
odrec <renatogarita@gmail.com>
2024-11-22 09:28:47 +0100

    Change connection string to connect inside docker container

--- metrics.py
+++ metrics.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 from datetime import datetime
 
 # Connect to MongoDB
-client = MongoClient('mongodb://localhost:27017/')
+client = MongoClient('mongodb://mongodb:27017/')
 db = client['LibreChat']
 users_collection = db['users']
 messages_collection = db['messages']
